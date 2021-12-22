25ea61f67f58453f4af2974e73d23cd99a31d792
matheus.oliveira <mrs555@outlook.com>
2021-12-22 09:09:55 -0300

    Classe funcionario criada

---
+++ src/main/java/br/com/livraria/controller/FuncionarioResource.java
@@ -0,0 +1,33 @@
+package br.com.livraria.controller;
+
+import br.com.livraria.model.Funcionario;
+import br.com.livraria.service.FuncionarioService;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+@Path("/funcionario")
+public class FuncionarioResource {
+
+    @Inject
+    FuncionarioService service;
+
+    @POST
+    @PermitAll
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(Funcionario funcionario){
+        service.adicionar(funcionario);
+        return  Response.created(URI.create("/funcionario")).build();
+    }
+
+    public Funcionario update(@PathParam("id") Long id, Person )
+}
