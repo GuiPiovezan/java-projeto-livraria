53db02bec345c65d2580cae6819d8bff587055c9
saraMontecino <sara_montecino@hotmail.com>
2021-12-22 11:44:21 -0300

    fornecedor criado

---
+++ src/main/java/br/com/livraria/controller/FornecedorResource.java
@@ -0,0 +1,57 @@
+package br.com.livraria.controller;
+
+import br.com.livraria.model.Fornecedor;
+import br.com.livraria.service.FornecedorService;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+@Path("/fornecedor")
+public class FornecedorResource {
+
+    @Inject
+    FornecedorService service;
+
+    @POST
+    @PermitAll
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response inserir(Fornecedor fornecedor) {
+        service.adiciona(fornecedor);
+        return Response.created(URI.create("/fornecedor")).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Fornecedor listarId(@PathParam("id") Long id){
+        return service.listarId(id);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Fornecedor> listar(){
+        return service.listar();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id){
+        service.deletar(id);
+    }
+
+    @PUT
+    @Transactional
+    @Path("{id}")
+    public Fornecedor alterar(@PathParam("id") Long id, Fornecedor fornecedor){
+        return service.altera(id, fornecedor);
+    }
+
+}
--- src/main/java/br/com/livraria/repository/FornecedorRepository.java
+++ src/main/java/br/com/livraria/repository/FornecedorRepository.java
@@ -1,10 +1,10 @@
 package br.com.livraria.repository;
 
-import br.com.livraria.model.Funcionario;
+import br.com.livraria.model.Fornecedor;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class FuncionarioRepository implements PanacheRepository<Funcionario> {
+public class FornecedorRepository implements PanacheRepository<Fornecedor> {
 }
