b973f4af11676b524e817bed505281940e805e95
fonsecaPedr0 <pedropbf5.pp@gmail.com>
2021-12-22 00:29:18 -0300

    implementando metodos

--- src/main/java/br/com/livraria/controller/JuridicaResource.java
+++ src/main/java/br/com/livraria/controller/JuridicaResource.java
@@ -6,12 +6,11 @@ import br.com.livraria.service.JuridicaService;
 import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.net.URI;
+import java.util.List;
 
 @Path("/juridica")
 public class JuridicaResource {
@@ -26,5 +25,26 @@ public class JuridicaResource {
     public Response inserir(Juridica juridica) {
         service.adiciona(juridica);
         return Response.created(URI.create("/juridica")).build();
+    }
+
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Juridica listarId(@PathParam("id") Long id){
+        return service.listarId(id);
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Juridica> listar(){
+        return service.listar();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id){
+        service.deletar(id);
     }
 }
--- src/main/java/br/com/livraria/service/JuridicaService.java
+++ src/main/java/br/com/livraria/service/JuridicaService.java
@@ -6,6 +6,7 @@ import br.com.livraria.repository.JuridicaRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.NotFoundException;
 import java.util.List;
 
 @ApplicationScoped
@@ -14,11 +15,32 @@ public class JuridicaService {
     @Inject
     JuridicaRepository juridicaRepository;
 
+    Juridica juridica;
+
     public void adiciona(Juridica juridica){
         juridicaRepository.persist(juridica);
     }
 
     public List<Juridica> listar(){
         return juridicaRepository.listAll();
+    }
+
+    public Juridica listarId(Long id){
+        return juridicaRepository.findById(id);
+    }
+
+
+    public void deletar(Long id){
+        juridica = juridicaRepository.findById(id);
+
+        if(juridica == null){
+            throw new NotFoundException();
+        }
+
+        juridicaRepository.deleteById(id);
+    }
+
+    public void alterar(){
+
     }
 }
