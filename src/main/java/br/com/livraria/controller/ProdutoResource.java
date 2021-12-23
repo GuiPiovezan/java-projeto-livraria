ccc91037c1c8acfb21f04836e03c1550efa35ac5
matheus.oliveira <mrs555@outlook.com>
2021-12-23 12:01:31 -0300

    Produto criado, não consegui testar os metodos listar por nome e tipo talvez estejam com problema.

---
+++ src/main/java/br/com/livraria/controller/ProdutoResource.java
@@ -0,0 +1,73 @@
+package br.com.livraria.controller;
+
+import br.com.livraria.model.Produto;
+import br.com.livraria.service.ProdutoService;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+@Path("/produtos")
+public class ProdutoResource {
+
+    @Inject
+    ProdutoService service;
+
+    @POST
+    @PermitAll
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response adicionar(Produto produto){
+        service.adicionar(produto);
+        return Response.created(URI.create("/produtos")).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Produto listarId(@PathParam("id") long id){
+        return service.listarId(id);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Produto> listar(){
+        return service.listar();
+    }
+
+    @GET
+    @Path("/search/{tipo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Produto> listarTipo(@PathParam("tipo") String tipo){
+        return service.listarTipo(tipo);
+    }
+
+    @GET
+    @Path("/search/{descricao}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Produto listarNomeProduto(@PathParam("descricao") String descricao){
+        return service.listarNomeProduto(descricao);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") long id){
+        service.deletar(id);
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Produto alterar(@PathParam("id") long id, Produto produto){
+        return service.alterar(id, produto);
+    }
+
+}
---
+++ src/main/java/br/com/livraria/service/ProdutoService.java
@@ -0,0 +1,68 @@
+package br.com.livraria.service;
+
+import br.com.livraria.model.Produto;
+import br.com.livraria.repository.ProdutoRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+
+@ApplicationScoped
+public class ProdutoService {
+
+    @Inject
+    ProdutoRepository produtoRepository;
+
+    @Inject
+    EntityManager entityManager;
+
+    public void adicionar(Produto produto){
+        produtoRepository.persist(produto);
+    }
+
+    public Produto listarId(long id){
+        return produtoRepository.findById(id);
+    }
+
+    public List<Produto> listar(){
+        return produtoRepository.listAll();
+    }
+
+    public List<Produto> listarTipo(String tipo){
+        Produto produto = new Produto();
+        return produtoRepository.list(tipo, produto.getTipoProduto());
+    }
+
+    public Produto listarNomeProduto(String descricao){
+        String descricaoAlt = descricao.replace("-"," ");
+        return entityManager.createQuery("FROM Produto WHERE nome = :nome", Produto.class).setParameter("nome", descricaoAlt).getSingleResult();
+    }
+
+    public void deletar(long id){
+        Produto produto = produtoRepository.findById(id);
+        if(produto == null){
+            throw new NotFoundException("O produto não existe!");
+        }
+
+        produtoRepository.deleteById(id);
+    }
+
+    public Produto alterar(long id, Produto produto){
+        Produto produtoUp = produtoRepository.findById(id);
+
+        if(produto == null){
+            throw new NotFoundException("O produto não existe!");
+        }
+
+        produtoUp.setDescricao(produto.getDescricao());
+        produtoUp.setQtdProduto(produto.getQtdProduto());
+        produtoUp.setTipoProduto(produto.getTipoProduto());
+        produtoUp.setPreco(produto.getPreco());
+
+        return produtoUp;
+    }
+
+
+}
